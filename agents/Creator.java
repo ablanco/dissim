0f5442ca1ff999436f0bbd5e756dcd8e8c1c7a5c
aleblaesc <aleblaesc@8c4acde5-3c00-4ed3-8754-bc08a9928e99>
2010-01-12 18:01:07 +0000

    Creadas las clases de los dos agentes del ejemplo del agua, y uno de los comportamientos. Falta dotarlos de funcionalidad.
    git-svn-id: https://forja.rediris.es/svn/cusl4-catastrof@4 8c4acde5-3c00-4ed3-8754-bc08a9928e99

---
+++ agents/Creator.java
@@ -0,0 +1,30 @@
+//    Flood and evacuation simulator using multi-agent technology
+//    Copyright (C) 2010 Alejandro Blanco and Manuel Gomar
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package agents;
+
+import jade.core.Agent;
+
+public class Creator extends Agent {
+
+	private static final long serialVersionUID = 1808039536880287251L;
+	
+	@Override
+	protected void setup() {
+		// TODO
+	}
+
+}
---
+++ behaviours/CreateAgent.java
@@ -0,0 +1,41 @@
+//    Flood and evacuation simulator using multi-agent technology
+//    Copyright (C) 2010 Alejandro Blanco and Manuel Gomar
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package behaviours;
+
+import jade.core.behaviours.OneShotBehaviour;
+import jade.wrapper.AgentController;
+import jade.wrapper.ControllerException;
+import jade.wrapper.PlatformController;
+
+public class CreateAgent extends OneShotBehaviour {
+
+	private static final long serialVersionUID = 1700469108759727145L;
+
+	@Override
+	public void action() {
+		Object[] arguments = new Object[]{"",""}; // TODO
+		PlatformController plataforma = myAgent.getContainerController();
+		AgentController agtctrl;
+		try {
+			agtctrl = plataforma.createNewAgent("B", "agents.Bernardo", arguments); // TODO
+			agtctrl.start();
+		} catch (ControllerException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
