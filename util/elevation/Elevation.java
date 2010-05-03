f4d3c31335469798b9ded95895ece9b6ff48e488
mangomaco <mangomaco@8c4acde5-3c00-4ed3-8754-bc08a9928e99>
2010-05-03 11:14:01 +0000

    Clase para mejorar al obtenicion de alturas
    git-svn-id: https://forja.rediris.es/svn/cusl4-catastrof@293 8c4acde5-3c00-4ed3-8754-bc08a9928e99

---
+++ util/elevation/Elevation.java
@@ -0,0 +1,13 @@
+package util.elevation;
+
+import util.Scenario;
+
+public class Elevation {
+	/**
+	 * Obtiene y escribe las alturas para el escenario scene
+	 * @param scene
+	 */
+	public static void getElevation(Scenario scene){
+		
+	}
+}
