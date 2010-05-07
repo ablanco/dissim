6ae3f2a7c5737bce824e25e0e2ac94135ad1d6a9
mangomaco <mangomaco@8c4acde5-3c00-4ed3-8754-bc08a9928e99>
2010-05-07 19:08:20 +0000

    He cambiado los colores de las personas para que se vea mejor.
    git-svn-id: https://forja.rediris.es/svn/cusl4-catastrof@317 8c4acde5-3c00-4ed3-8754-bc08a9928e99

--- kml/KmlPeople.java
+++ kml/KmlPeople.java
@@ -27,7 +27,9 @@ import de.micromata.opengis.kml.v_2_2_0.Placemark;
 public class KmlPeople {
 
 	public final static String imgPath = "http://pfc.mensab.com/wp-content/uploads/2010/03/";
-	private final String peopleId = "pedestrian"; 
+	public final static String RUNNING = "running";
+	public final static String SAFE = "safe";
+	public final static String DEAD = "dead";
 	private Folder container;
 	private Folder folder;
 	private String begin;
@@ -38,69 +40,93 @@ public class KmlPeople {
 		createPeopleStyle();
 	}
 
-	protected void createPeopleStyle() {		
-		String abgr = "ff" + "00" + "00" + "ff";		
-		container.createAndAddStyle().withId(peopleId)
-				.createAndSetPolyStyle().withColor(abgr);
+	protected void createPeopleStyle() {
+		// Verde
+		String safeABGR = "ff" + "00" + "ff" + "00";
+		// Gris
+		String deadABGR = "ff" + "aa" + "aa" + "aa";
+		// Amarillo
+		String runningABGR = "ff" + "00" + "ff" + "f0";
+		container.createAndAddStyle().withId(RUNNING).createAndSetPolyStyle()
+				.withColor(runningABGR);
+		container.createAndAddStyle().withId(SAFE).createAndSetPolyStyle()
+				.withColor(safeABGR);
+		container.createAndAddStyle().withId(DEAD).createAndSetPolyStyle()
+				.withColor(deadABGR);
 		// polyStyle.setColorMode(ColorMode.NORMAL);
 	}
-	
-	public void update(List<Pedestrian> people, String name,String beginTime,
+
+	public void update(List<Pedestrian> people, String name, String beginTime,
 			String endTime, double[] incs) {
 		folder = container.createAndAddFolder().withName(name).withDescription(
 				"From: " + beginTime + " To :" + endTime);
-		this.begin=beginTime;
-		this.end=endTime;
+		this.begin = beginTime;
+		this.end = endTime;
 		double ilat = incs[0] * 4 / 6;
 		double ilng = incs[1] / 2;
 		for (Pedestrian pedestrian : people) {
-		ArrayList<LatLng> rawPolygon = new ArrayList<LatLng>();
-		rawPolygon.add(pedestrian.getPos());
-			drawPedestrian(new Kpolygon(Kpolygon.Pedestrian, rawPolygon, ilat, ilng));
+			ArrayList<LatLng> rawPolygon = new ArrayList<LatLng>();
+			rawPolygon.add(pedestrian.getPos());
+			drawPedestrian(new Kpolygon(Kpolygon.Pedestrian, rawPolygon, ilat,
+					ilng), pedestrian.getStatus());
 		}
 	}
 
-//	public void drawPedestrian(Pedestrian pedestrian, String begin, String end,
-//			double[] incs) {
-//		GroundOverlay groundoverlay = folder.createAndAddGroundOverlay()
-//				.withAltitudeMode(AltitudeMode.RELATIVE_TO_GROUND);
-//		KmlBase.setTimeSpan(groundoverlay, begin, end);
-//		// Depending status set propper img
-//		Icon icon = groundoverlay.createAndSetIcon().withRefreshInterval(10.0).withViewBoundScale(1.0).withViewRefreshTime(10.0).withRefreshMode(RefreshMode.ON_INTERVAL).withViewRefreshMode(ViewRefreshMode.ON_STOP);
-//		switch (pedestrian.getStatus()) {
-//		case Pedestrian.HEALTHY:
-//			icon.withHref(imgPath + "healthy.png");
-//			break;
-//		case Pedestrian.HURT:
-//			icon.withHref(imgPath + "hurt.png");
-//			break;
-//		case Pedestrian.DEAD:
-//			icon.withHref(imgPath + "dead.png");
-//			break;
-//		default:
-//			break;
-//		}
-//		// Setting LatLonBox
-//		LatLng pos = pedestrian.getPos();
-//		groundoverlay.createAndSetLatLonBox().withNorth(pos.getLat() + incs[0])
-//				.withSouth(pos.getLat() - incs[0]).withEast(
-//						pos.getLng() + incs[1])
-//				.withWest(pos.getLng() - incs[1]).withRotation(45d);
-//		
-//	}
+	// public void drawPedestrian(Pedestrian pedestrian, String begin, String
+	// end,
+	// double[] incs) {
+	// GroundOverlay groundoverlay = folder.createAndAddGroundOverlay()
+	// .withAltitudeMode(AltitudeMode.RELATIVE_TO_GROUND);
+	// KmlBase.setTimeSpan(groundoverlay, begin, end);
+	// // Depending status set propper img
+	// Icon icon =
+	// groundoverlay.createAndSetIcon().withRefreshInterval(10.0).withViewBoundScale(1.0).withViewRefreshTime(10.0).withRefreshMode(RefreshMode.ON_INTERVAL).withViewRefreshMode(ViewRefreshMode.ON_STOP);
+	// switch (pedestrian.getStatus()) {
+	// case Pedestrian.HEALTHY:
+	// icon.withHref(imgPath + "healthy.png");
+	// break;
+	// case Pedestrian.HURT:
+	// icon.withHref(imgPath + "hurt.png");
+	// break;
+	// case Pedestrian.DEAD:
+	// icon.withHref(imgPath + "dead.png");
+	// break;
+	// default:
+	// break;
+	// }
+	// // Setting LatLonBox
+	// LatLng pos = pedestrian.getPos();
+	// groundoverlay.createAndSetLatLonBox().withNorth(pos.getLat() + incs[0])
+	// .withSouth(pos.getLat() - incs[0]).withEast(
+	// pos.getLng() + incs[1])
+	// .withWest(pos.getLng() - incs[1]).withRotation(45d);
+	//		
+	// }
 
 	/**
 	 * Dibuja un poligno que representa a una persona
 	 */
-	public void drawPedestrian(Kpolygon kp) {
+	public void drawPedestrian(Kpolygon kp, int status) {
 		if (kp == null) {
 			throw new IllegalArgumentException("El polygono no puede ser nulo");
 		}
 		Placemark placeMark = KmlBase.newPlaceMark(folder, String.valueOf(kp
 				.getDeep()));
 		KmlBase.setTimeSpan(placeMark, begin, end);
-		placeMark.setStyleUrl(peopleId);
+		switch (status) {
+		case Pedestrian.DEAD:
+			placeMark.setStyleUrl(DEAD);
+			break;
+		case Pedestrian.SAFE:
+			placeMark.setStyleUrl(SAFE);
+			break;
+		case Pedestrian.HEALTHY:
+			placeMark.setStyleUrl(RUNNING);
+			break;
+		default:
+			break;
+		}
 		KmlBase.drawPolygon(placeMark, kp);
-		
+
 	}
 }
